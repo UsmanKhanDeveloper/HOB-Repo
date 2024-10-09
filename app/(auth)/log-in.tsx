6f2f5c0061e33f27d36f38da1df6ad2c5f1f959f
Aryan480 <aryanmwadi1@outlook.com>
2024-10-08 22:26:49 -0400

    Implemented Login UI screen

--- app/(auth)/log-in.tsx
+++ app/(auth)/log-in.tsx
@@ -1,11 +1,70 @@
-import {Text} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import { images, icons } from "@/constants";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import {GestureResponderEvent, Image, ScrollView,Text, View} from "react-native";
 
 const LogIn =() => {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+      });
+    
+
+    function onSignInPress(event: GestureResponderEvent): void {
+        throw new Error("Function not implemented.");
+    }
+
     return(
-        <SafeAreaView>
-            <Text>Log In</Text>
-        </SafeAreaView>
+        <ScrollView className="flex-1 bg-white">
+        <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.houseHeader} className="z-0 w-full h-[250px]"/>
+          <View className="bottom-70 items-center">
+            <Text className="text-3xl text-black font-JakartaBold text-center text-[#0286FF]">
+              Welcome to House of Beginners
+            </Text>
+          </View>
+        </View>
+
+  
+          <View className="p-5 top-20">
+            <InputField
+              label="Email"
+              placeholder="Enter email"
+              icon={icons.email}
+              textContentType="emailAddress"
+              value={form.email}
+              onChangeText={(value) => setForm({ ...form, email: value })}
+            />
+  
+            <InputField
+              label="Password"
+              placeholder="Enter password"
+              icon={icons.lock}
+              secureTextEntry={true}
+              textContentType="password"
+              value={form.password}
+              onChangeText={(value) => setForm({ ...form, password: value })}
+            />
+  
+            <CustomButton
+              title="Log In"
+              onPress={onSignInPress}
+              className="mt-6"
+            />
+  
+            <Link
+              href="/sign-up"
+              className="text-lg text-center text-general-200 mt-10"
+            >
+              Don't have an account?{" "}
+              <Text className="text-primary-500">Sign Up</Text>
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
     );
 };
 
