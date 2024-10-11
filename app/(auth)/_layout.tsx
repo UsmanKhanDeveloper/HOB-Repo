9371f8b7ef4edf8a4567838f2aad097c8c59bc92
Aryan480 <aryanmwadi1@outlook.com>
2024-10-11 04:45:46 -0400

    removed "welcome route"

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,11 +4,8 @@ const Layout = () =>{
 
   return (
       <Stack>
-        <Stack.Screen name="welcome" options={{ headerShown: false }} />
         <Stack.Screen name="log-in" options={{ headerShown: false }} />
         <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-
-
       </Stack>
   );
 }
