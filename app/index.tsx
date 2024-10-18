73c4a9f929c12fe459dee82c6eb4c8939cb4a905
Aryan480 <aryanmwadi1@outlook.com>
2024-10-18 02:20:59 -0400

    fixed react import error

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { Redirect } from "expo-router";
 import {Text, View} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import React from 'react';
+
 
 const Home =() => {
     return <Redirect href="/(auth)/log-in"/>;
