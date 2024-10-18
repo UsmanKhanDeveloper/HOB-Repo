6554cec5aeb0a244f17bed03aae5df37bcddceb0
Aryan480 <aryanmwadi1@outlook.com>
2024-10-18 17:33:26 -0400

    changed route to sign-in

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,16 @@ import { Redirect } from "expo-router";
 import {Text, View} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react';
+import { useAuth } from "@clerk/clerk-expo";
 
 
 const Home =() => {
-    return <Redirect href="/(auth)/log-in"/>;
+    const { isSignedIn } = useAuth()
+
+  if (isSignedIn) {
+    return <Redirect href='/(root)/(tabs)/guides'/> //change and redirect to home page
+  }
+    return <Redirect href="/(auth)/sign-up"/>;
 };
 
 export default Home;
