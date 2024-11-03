6dc42f5f8b993e97df2b712b05849a906decab4d
Usman Khan <khan356@sheridancollege.ca>
2024-11-03 13:41:39 -0500

    some basic syntax clean up, no changes

--- app/(auth)/guide-Buying.tsx
+++ app/(auth)/guide-Buying.tsx
@@ -1,20 +1,29 @@
-import React, {useRef, useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, ScrollView, Image, FlatList, Linking, Animated} from 'react-native';
-import {useRouter} from 'expo-router';
-import {images} from "@/constants";
-import {SafeAreaView} from "react-native-safe-area-context";
-import StepOnePrePurchasing from '@/app/(auth)/StepOnePrePurchasing';
+import React, { useRef, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  FlatList,
+  Linking,
+  Animated,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { images } from "@/constants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import StepOnePrePurchasing from "@/app/(auth)/StepOnePrePurchasing";
 import StepTwoUnderstandingMarketCondition from "@/app/(auth)/StepTwoUnderstandingMarketCondition";
 import StepThreeFindingAHome from "@/app/(auth)/StepThreeFindingAHome";
 import StepFourMakingAnOffer from "@/app/(auth)/StepFourMakingAnOffer";
 import StepFivePostPurchase from "@/app/(auth)/StepFivePostPurchase";
 
 interface Question {
-    text: string;
-    options: string[];
-    correctAnswer: string;
-    correctExplanation: string;
-    incorrectExplanation: string;
+  text: string;
+  options: string[];
+  correctAnswer: string;
+  correctExplanation: string;
+  incorrectExplanation: string;
 }
 
 // // Define subStepsOne outside of the component
@@ -1282,39 +1291,46 @@ interface Question {
 //     );
 // };
 
-
 const guideBuying = () => {
-    const router = useRouter();
+  const router = useRouter();
 
+  const goToNextStep = (nextStep: string) => {
+    router.push(nextStep as any); // Ensure this path is correct
+  };
 
-    const goToNextStep = (nextStep: string) => {
-        router.push(nextStep as any); // Ensure this path is correct
-    };
+  return (
+    <>
+      {/* Title View */}
+      <SafeAreaView className="w-full bg-white py-4">
+        <Text className="text-2xl font-bold text-center text-blue-800">
+          Steps For Buying A Home
+        </Text>
+      </SafeAreaView>
 
-
-    return (
-        <>
-            {/* Title View */}
-            <SafeAreaView className="w-full bg-white py-4">
-                <Text className="text-2xl font-bold text-center text-blue-800">
-                    Steps For Buying A Home
-                </Text>
-            </SafeAreaView>
-
-            {/* Content Scroll View */}
-            <ScrollView className="flex-1 bg-white">
-                <Text className="text-lg text-center text-blue-700 mb-5 mt-3">
-                    ‣ Navigate Each Step Answering Questions to Track Your Progress
-                </Text>
-                {/* Step Components below */}
-                <StepOnePrePurchasing goToNextStep={() => goToNextStep('StepTwoPrePurchasing')} />
-                <StepTwoUnderstandingMarketCondition goToNextStep={() => goToNextStep('StepThreeFindingAHome')} />
-                <StepThreeFindingAHome goToNextStep={() => goToNextStep('StepFourMakingAnOfferr')} />
-                <StepFourMakingAnOffer goToNextStep={() => goToNextStep('StepFivePostPurchase')} />
-                <StepFivePostPurchase goToNextStep={() => goToNextStep('StepOnePrePurchasing  ')} />
-            </ScrollView>
-        </>
-    );
+      {/* Content Scroll View */}
+      <ScrollView className="flex-1 bg-white">
+        <Text className="text-lg text-center text-blue-700 mb-5 mt-3">
+          ‣ Navigate Each Step Answering Questions to Track Your Progress
+        </Text>
+        {/* Step Components below */}
+        <StepOnePrePurchasing
+          goToNextStep={() => goToNextStep("StepTwoPrePurchasing")}
+        />
+        <StepTwoUnderstandingMarketCondition
+          goToNextStep={() => goToNextStep("StepThreeFindingAHome")}
+        />
+        <StepThreeFindingAHome
+          goToNextStep={() => goToNextStep("StepFourMakingAnOfferr")}
+        />
+        <StepFourMakingAnOffer
+          goToNextStep={() => goToNextStep("StepFivePostPurchase")}
+        />
+        <StepFivePostPurchase
+          goToNextStep={() => goToNextStep("StepOnePrePurchasing")}
+        />
+      </ScrollView>
+    </>
+  );
 };
 
 export default guideBuying;
