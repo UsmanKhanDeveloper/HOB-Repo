21e40d11b9f3c9fb1bb76998775c391aaf8c6b75
Usman Khan <khan356@sheridancollege.ca>
2024-11-09 02:08:27 -0500

    removed some pages from (auth)/_layout.tsx because guides pages were moved into their own section in the project file structure

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,22 +1,13 @@
-import { Stack } from 'expo-router';
-import React from 'react';
+import { Stack } from "expo-router";
+import React from "react";
 
-
-const Layout = () =>{
-
+const Layout = () => {
   return (
-      <Stack>
-        <Stack.Screen name="log-in" options={{ headerShown: false }} />
-        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-          <Stack.Screen name="guides" options={{ headerShown: false }} />
-          <Stack.Screen name="guide-Buying" options={{ headerShown: false }} />
-        <Stack.Screen name="StepOnePrePurchase" options={{ headerShown: false }} />
-          <Stack.Screen name="StepTwoUnderstandingMarketCondition" options={{ headerShown: false }} />
-          <Stack.Screen name="StepThreeFindingAHome" options={{ headerShown: false }} />
-          <Stack.Screen name="StepFourMakingAnOffer" options={{ headerShown: false }} />
-          <Stack.Screen name="StepFivePostPurchase" options={{ headerShown: false }} />
-      </Stack>
-
+    <Stack>
+      <Stack.Screen name="log-in" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="guides" options={{ headerShown: false }} />
+    </Stack>
   );
-}
+};
 export default Layout;
