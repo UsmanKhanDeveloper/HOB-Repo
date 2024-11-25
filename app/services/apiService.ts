3f42e49fdfc0c05df0d00be905d1dc1aadbf5389
Bon giovi <giovicobe@yahoo.com>
2024-11-25 02:46:37 -0500

    began connecting the user's input from the search bar to a search function I made in the API.

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,14 +1,14 @@
 
-const API_HOST = "zillow-com1.p.rapidapi.com"
+const API_HOST = "zillow56.p.rapidapi.com"
 const API_KEY= process.env.REACT_APP_RAPIDAPI_KEY;
 
 export const fetchPropertyData = async (location: string) => {
     try {
-      const response = await fetch(`https://${API_HOST}/property?location=${location}`, {
+      const response = await fetch(`https://${API_HOST}/search?location=${}&output=json&status=forSale&sortSelection=priorityscore&listing_type=by_agent&doz=any`, {
         method: 'GET',
         headers: {
-          'X-RapidAPI-Key': API_KEY!,
-          'X-RapidAPI-Host': API_HOST,
+          'x-rapidapi-key': API_KEY!,
+          'x-rapidapi-host': API_HOST,
         },
       });
   
@@ -17,9 +17,17 @@ export const fetchPropertyData = async (location: string) => {
       }
   
       const data = await response.json();
-      return data;
+      return mapSearchResults(data.results);
     } catch (error) {
       console.error("Error fetching property data:", error);
       throw error;
     }
+  };
+
+  // mapping processes the list and returns a copy of that list with the parametppa you specified inside here 
+  const mapSearchResults = (searchResults: any[]) => {
+    return searchResults.map((result: any) => ({
+      city: result.city,
+      country: result.country 
+      }));
   };
